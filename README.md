7f0ecaef5a3000f0ac8dd91ea9744e65fe8b7cac
WslyStvnChng <mr.cheng.st@gmail.com^C>
2018-04-03 11:24:52 -0600

    first commit, made a boilplate for server.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Scraping-News-Site
+
+#Heroku mLab URI: mongodb://heroku_3h5kpclp:otg572npmm838k7h46t442u0kf@ds145997.mlab.com:45997/heroku_3h5kpclp
---
+++ server.js
@@ -0,0 +1,78 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(
+  // "mongodb://heroku_3h5kpclp:otg572npmm838k7h46t442u0kf@ds145997.mlab.com:45997/heroku_3h5kpclp",
+  "mongodb://localhost/Scraping-News-Site",
+  {
+    useMongoClient: true
+  }
+);
+
+// ******
+// ROUTES
+// ******
+
+
+// A GET route for scraping the echojs website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  axios.get("https://www.theverge.com/tech/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article
+        .create(result)
+        .then(function(dbArticle) {
+          // If we were able to successfully scrape and save an Article, send a message to the client
+          res.send("Scrape Complete");
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          res.json(err);
+        });
+    });
+  });
+});
